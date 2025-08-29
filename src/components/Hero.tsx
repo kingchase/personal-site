3b47928e8d92e0226682225b664f2f9d55827291
Chase King <chasemking@gmail.com>
2025-08-29 17:01:55 -0400

    Placeholder image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,20 @@
 'use client';
 import { useTranslation } from "react-i18next";
+import Image from 'next/image'
 
 export const Hero = () => {
     const { t } = useTranslation();
     return (
         <div className="flex flex-col gap-2">
-            {/* Image */}
-            {/* Text */}
-            <h2>{t('home:name')}</h2>
+            <div className="mx-auto">
+                <Image
+                    className="rounded-lg"
+                    src="/images/chase.webp"
+                    alt="Chase King"
+                    width={400}
+                    height={300} />
+            </div>
+            <h2 className="">{t('home:name')}</h2>
             <p>{t('home:intro.detailed')}</p>
         </div>
     );
