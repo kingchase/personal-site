a9a28c20d5695f7ac941aa2b3b273dde21712420
Chase King <chasemking@gmail.com>
2025-09-28 18:00:21 -0400

    Removed career page and justify navbar center

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,10 @@ export const Navbar = () => {
     const { t } = useTranslation();
     return (
         <nav className="px-4 py-4">
-            <ul className="flex flex-row gap-8 md:gap-6">
+            <ul className="flex flex-row gap-8 md:gap-6 justify-center">
                 <li>
                     <a href="/" className="text-lg font-medium hover:underline">
                         {t('common:nav.home')}
-                    </a>
-                </li>
-                <li>
-                    <a href="/career" className="text-lg font-medium hover:underline">
-                        {t('common:nav.career')}
                     </a>
                 </li>
                 <li>
